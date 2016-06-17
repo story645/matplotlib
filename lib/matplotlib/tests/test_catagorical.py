10e2a2356e11f04c0d62daae6e04a3b6e94edd2e
hannah <story645@gmail.com>
2016-06-17 16:37:07 -0400

    didn't break factorize (yet) and can import without messing up mpl imports

---
+++ lib/matplotlib/tests/test_catagorical.py
@@ -0,0 +1,44 @@
+"""This will be factored out into different functions (and possibly libraries) 
+but I wanted to start with a catch all place for simplicity's sake
+
+#testing implemenation of native (registered) support for pandas 
+catagorical dataframe objects 
+"""
+
+
+import nose.tools
+from nose.plugins.skip import SkipTest
+
+import numpy as np
+
+
+import importlib.util
+spec = importlib.util.spec_from_file_location("matplotlib.categorical", "../categorical.py")
+cat = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(cat)
+
+
+
+def test_factorize():
+    index, labels = cat.factorize([1,1,2,2,1,1,4,5,6])
+    np.testing.assert_equal(index, np.array([0, 0, 1, 1, 0, 0, 2, 3, 4]))
+    np.testing.assert_equal(labels, np.array([1, 2, 4, 5, 6]))
+
+@SkipTest
+def test_CategoricalLocator():
+    loc = cat.CategoricalLocator()
+    
+
+@SkipTest
+def test_CategoricalFormatter():
+    class FakeAxis(object):
+        """Allow Formatter to be called without having a "full" plot set up."""
+        def __init__(self, vmin=1, vmax=10):
+            self.vmin = vmin
+            self.vmax = vmax
+
+        def get_view_interval(self):
+            return self.vmin, self.vmax
+    
+    formatter = cat.CategoricalFormatter()
+    formatter.axis = FakeAxis()
