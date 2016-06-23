08fef8c85b057901edf617edb73dfd23f4186694
hannah <story645@gmail.com>
2016-06-23 02:44:10 -0400

    axis data_unit & conversion call stack

--- lib/matplotlib/categorical.py
+++ lib/matplotlib/categorical.py
@@ -1,6 +1,8 @@
 """
 catch all for categorical functions
 """
+from collections import OrderedDict
+
 import six
 import numpy as np
 
@@ -17,56 +19,58 @@ def register():
     elif six.PY2:
         units.registry[basestring] = CategoricalConverter()
 
-    # units.registry[pandas.Categorical] = CategoricalConverter()
-
 
 class CategoricalConverter(units.ConversionInterface):
+
     @staticmethod
     def convert(value, unit, axis):
         if isinstance(value, six.string_types):
             return 0
-
+        
         vals = np.asarray(value, dtype='str')
-        uniq = np.unique(vals)
-
-        if 'nan' in uniq:
-            vals[vals == 'nan'] = -1
-            uniq = uniq[uniq != 'nan']
-
-        if '-inf' in uniq:
-            vals[vals == '-inf'] = uniq.shape[0] - 1
-            uniq = uniq[uniq != '-inf']
-
-        if 'inf' in uniq:
-            vals[vals == 'inf'] = uniq.shape[0] - 1
-            uniq = uniq[uniq != 'inf']
-
-        vmap = dict(zip(uniq, list(range(uniq.shape[0]))))
-
-        for u in uniq:
-            vals[vals == u] = vmap[u]
+        for label, loc  in axis.unit_data.items():
+            vals[vals == label] = loc
 
         return vals.astype('int')
 
     @staticmethod
     def axisinfo(unit, axis):
-        majloc = CategoricalFormatter()
-        majfmt = CategoricalLocator()
+        seq, locs = zip(*axis.unit_data.items())
+        majloc = CategoricalFormatter(locs)
+        majfmt = CategoricalLocator(seq)
         return units.AxisInfo(majloc=majloc, majfmt=majfmt, label=None)
 
     @staticmethod
-    def default_units(x, axis):
-        """Default unit for categories is none"""
-        """but if x is a dictionary then the default is a key"""
+    def default_units(data, axis):
+        """map is built here because the conversion call stack is:
+        default_units->axis info->convert
+        """
+
+        # factor this stuff out so I can test it
+        vals = np.asarray(data, dtype='str')
+        uniq = np.unique(vals)
+
+        # pandas factorize convention
+        if 'nan' in uniq:
+            vals[vals == 'nan'] = -1
+            uniq = uniq[uniq != 'nan']
+
+        for inf in ['-inf', 'inf']:
+            if inf in uniq:
+                vals[vals == inf] = uniq.shape[0] - 1
+                uniq = uniq[uniq != inf]
+
+        index = list(range(uniq.shape[0]))
+        axis.unit_data = OrderedDict(zip(uniq, index))
+
         return None
 
 
 class CategoricalLocator(ticker.FixedLocator):
-    """Starting with fixed because all catagories should be shown
-    """
-    pass
+    def __init__(self, locs):
+        super(CategoricalLocator, self).__init__(locs)
 
 
 class CategoricalFormatter(ticker.FixedFormatter):
-    """Probably a wrapper on one of the string formatters"""
-    pass
+    def __init__(self, seq):
+        super(CategoricalFormatter, self).__init__(seq)
